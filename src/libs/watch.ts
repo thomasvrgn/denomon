532cf96102016c2b0b574d366272be9064467926
NessMC <contact@nessmc.fr>
2020-05-31 14:15:45 +0100

    ✨ Extension checker added.

--- src/libs/watch.ts
+++ src/libs/watch.ts
@@ -4,6 +4,25 @@ import { File } from '../typings/file.ts';
 
 export async function watch() {
   const files: File[] = [];
-  for await (const file of expandGlob(configuration.pattern || '*.*')) files.push(file);
+  for await (const file of expandGlob(configuration.pattern || '*.*')) {
+    if (!file.isFile) continue;
+    if (!configuration.extensions) {
+      files.push(file);
+      continue;
+    }
+    const extension: string = file.path
+      .split('/')
+      .slice(-1)[0]
+      .split('.')
+      .slice(1)
+      .join('.');
+    let match: boolean = false;
+    for (const configExtension of configuration.extensions) {
+      if (extension.match(configExtension)) {
+        match = true;
+      }
+    }
+    if (match) files.push(file);
+  }
   console.log(files);
 }
