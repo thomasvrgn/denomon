6866fde771890b48862b9d3c9d13e914e2ece3be
NessMC <contact@nessmc.fr>
2020-10-16 19:18:02 +0200

    ♻️ Configuration in logging methods refactored.

--- src/libs/log.ts
+++ src/libs/log.ts
@@ -1,15 +1,16 @@
 import * as color from 'https://deno.land/std/fmt/colors.ts';
+import { configuration } from './config.ts';
 
 export class Log {
   public static error(...message: string[]): void {
-    console.log(color.red(`[denomon] ${message.join(' ')}`));
+    console.log(color.red(`[${configuration.name}] ${message.join(' ')}`));
   }
 
   public static warning(...message: string[]): void {
-    console.log(color.yellow(`[denomon] ${message.join(' ')}`));
+    console.log(color.yellow(`[${configuration.name}] ${message.join(' ')}`));
   }
 
   public static success(...message: string[]): void {
-    console.log(color.green(`[denomon] ${message.join(' ')}`));
+    console.log(color.green(`[${configuration.name}] ${message.join(' ')}`));
   }
 }
