cd9ad75f56d530f97ba6ef10826f9e12a145694f
NessMC <contact@nessmc.fr>
2020-10-16 18:44:38 +0200

    ✨ Warning logging method added.

--- src/libs/log.ts
+++ src/libs/log.ts
@@ -4,4 +4,8 @@ export class Log {
   public static error(...message: string[]): void {
     console.log(color.red(`[denomon] ${message.join(' ')}`));
   }
+
+  public static warning(...message: string[]): void {
+    console.log(color.yellow(`[denomon] ${message.join(' ')}`));
+  }
 }
